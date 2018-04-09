c573d51a183ba4afcf1e978ec685435f099dc27c
samialab <sami.alabed@student.manchester.ac.uk>
2018-04-09 17:22:24 +0100

    Add dl4j helpers and NN LinearRegression sample

---
+++ src/main/comp34120/ex2/LinearRegressionLeader.java
@@ -0,0 +1,58 @@
+package comp34120.ex2;
+
+import comp34120.ex2.Utils.NeuralNetUtil;
+import comp34120.ex2.Utils.PlatformUtil;
+import comp34120.ex2.regression.LinearRegression;
+import comp34120.ex2.regression.Regression;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+// Linear Regression leader using NN
+final class LinearRegressionLeader extends PlayerImpl {
+    private final Regression regression;
+    private final static float UNIT_COST = 1.0f;
+
+    public LinearRegressionLeader() throws RemoteException, NotBoundException {
+        super(PlayerType.LEADER, "Linear Regression NN Leader");
+        this.regression = new LinearRegression(new NeuralNetUtil(PlatformUtil.getAllRecordsUntilDay(platform, 100)));
+    }
+
+    @Override
+    public void goodbye() throws RemoteException {
+        super.goodbye();
+    }
+
+    /**
+     * @param simulationSteps Indicates how many steps will the simulation perform
+     * @throws RemoteException If the connection fails, the JVM will throw a RemoteException
+     */
+    @Override
+    public void startSimulation(int simulationSteps) throws RemoteException {
+        super.startSimulation(simulationSteps);
+    }
+
+    /**
+     * To inform this instance to proceed to a new simulation day
+     *
+     * @param newDate The date of the new day
+     * @throws RemoteException If the connection fails, the JVM will throw a RemoteException
+     */
+    @Override
+    public void proceedNewDay(int newDate) throws RemoteException {
+        Record lastDayRecord = platform.query(playerType, newDate - 1);
+        regression.update(lastDayRecord);
+        platform.log(playerType, "Last day profit " + PlatformUtil.computeProfit(lastDayRecord));
+
+        // TODO(samialab): Implement price function
+        int leaderNewPrice = 2;
+        float predictedFollowerPrice = regression.predictFollowerPrice(newDate, leaderNewPrice, UNIT_COST);
+        platform.log(playerType, "Predicted follower price: " + predictedFollowerPrice);
+
+        platform.publishPrice(playerType, leaderNewPrice);
+    }
+
+    public static void main(final String[] p_args) throws RemoteException, NotBoundException {
+        new LinearRegressionLeader();
+    }
+}
---
+++ src/main/comp34120/ex2/regression/LinearRegression.java
@@ -0,0 +1,79 @@
+package comp34120.ex2.regression;
+
+import comp34120.ex2.Utils.NeuralNetUtil;
+import comp34120.ex2.Record;
+import org.deeplearning4j.datasets.iterator.impl.ListDataSetIterator;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+public class LinearRegression implements Regression {
+    // Random number generator seed, for reproducibility
+    private static final int seed = 12345;
+    // Number of epochs (full passes of the data)
+    private static final int nEpochs = 2;
+    // Network learning rate
+    private static final double learningRate = 0.01;
+    // Create the network
+    private static final int numInput = 3;
+    private static final int numOutputs = 1;
+    private static final int nHidden = 1;
+
+
+    private final MultiLayerNetwork NEURAL_NETWORK;
+    private final NeuralNetUtil neuralNetUtil;
+
+    @Override
+    public float predictFollowerPrice(int day, float leaderPrice, float unitCost) {
+        INDArray inputFeatureVector = neuralNetUtil.createInputFeatureVector(day, leaderPrice, unitCost);
+        INDArray predictedPrice = NEURAL_NETWORK.output(inputFeatureVector, false);
+        return predictedPrice.getFloat(0);
+    }
+
+    @Override
+    public void update(Record record) {
+        neuralNetUtil.updateFeaturesWithRecord(record);
+
+        DataSetIterator trainingDataIterator = new ListDataSetIterator<>(neuralNetUtil.getTrainingDataSet().asList());
+        // Train the network on the full data set, and evaluate in periodically
+        for (int i = 0; i < nEpochs; i++) {
+            trainingDataIterator.reset();
+            NEURAL_NETWORK.fit(trainingDataIterator);
+        }
+    }
+
+    public LinearRegression(NeuralNetUtil neuralNetUtil) {
+        this.neuralNetUtil = neuralNetUtil;
+        // TODO(samialab): configure the network properly
+        NEURAL_NETWORK = new MultiLayerNetwork(new NeuralNetConfiguration.Builder()
+                                                       .seed(seed)
+                                                       .weightInit(WeightInit.XAVIER)
+                                                       .updater(new Nesterovs(learningRate, 0.9))
+                                                       .list()
+                                                       .layer(0,
+                                                              new DenseLayer.Builder().nIn(numInput)
+                                                                                      .nOut(nHidden)
+                                                                                      .activation(Activation.TANH)
+                                                                                      .build())
+                                                       .layer(1,
+                                                              new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
+                                                                      .activation(Activation.IDENTITY)
+                                                                      .nIn(nHidden).nOut(numOutputs)
+                                                                      .build())
+                                                       .pretrain(false)
+                                                       .backprop(true)
+                                                       .build()
+        );
+        NEURAL_NETWORK.init();
+        NEURAL_NETWORK.setListeners(new ScoreIterationListener(1));
+
+    }
+}
