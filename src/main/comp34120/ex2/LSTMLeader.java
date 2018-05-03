dbda4e4d87f19638472a61b6c028ef9587d51426
samialab <sami.alabed@student.manchester.ac.uk>
2018-05-03 10:55:53 +0100

    Add LTSM

--- src/main/comp34120/ex2/LSTMLeader.java
+++ src/main/comp34120/ex2/LSTMLeader.java
@@ -1,9 +1,8 @@
 package comp34120.ex2;
 
-import comp34120.ex2.maximizer.AnalyticPayoffMaximizer;
 import comp34120.ex2.maximizer.PayoffMaximizer;
 import comp34120.ex2.maximizer.SimplePayoffMaximizer;
-import comp34120.ex2.regression.LinearRegression;
+import comp34120.ex2.regression.LSTMRegression;
 import comp34120.ex2.regression.Regression;
 import comp34120.ex2.utils.NeuralNetUtil;
 import comp34120.ex2.utils.PlatformUtil;
@@ -13,13 +12,13 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 // Linear Regression leader using NN
-final class LinearRegressionLeader extends PlayerImpl {
+final class LSTMLeader extends PlayerImpl {
     private PayoffMaximizer maximizer;
     private StatisticalCollector collector;
     private Regression regression;
 
-    public LinearRegressionLeader() throws RemoteException, NotBoundException {
-        super(PlayerType.LEADER, "Linear Regression NN Leader");
+    public LSTMLeader() throws RemoteException, NotBoundException {
+        super(PlayerType.LEADER, "LSTM Leader");
     }
 
     /**
@@ -28,8 +27,8 @@ final class LinearRegressionLeader extends PlayerImpl {
      */
     @Override
     public void startSimulation(int simulationSteps) throws RemoteException {
-        this.regression = new LinearRegression(new NeuralNetUtil(PlatformUtil.getAllRecordsUntilDay(platform, 100)));
-        this.maximizer = new AnalyticPayoffMaximizer(regression);
+        this.regression = new LSTMRegression(new NeuralNetUtil(PlatformUtil.getAllRecordsUntilDay(platform, 100)));
+        this.maximizer = new SimplePayoffMaximizer(regression);
         this.collector = new StatisticalCollector();
         super.startSimulation(simulationSteps);
     }
@@ -63,6 +62,6 @@ final class LinearRegressionLeader extends PlayerImpl {
     }
 
     public static void main(final String[] p_args) throws RemoteException, NotBoundException {
-        new LinearRegressionLeader();
+        new LSTMLeader();
     }
 }
